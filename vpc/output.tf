58021744db4813f3874f0205ec21ba69555a95a0
innocent <innocentgodwin580@yahoo.com>
2025-08-19 11:06:04 +0930

    nat-gateway module created

--- vpc/output.tf
+++ vpc/output.tf
@@ -24,31 +24,31 @@ output "internet_gateway" {
 }
 
 # export the public subnet az1a id
-output "public_subnet_az1a" {
+output "public_subnet_az1a_id" {
     value = aws_subnet.public_subnet_az1a.id 
 }
 
 #  export the public subnet az1b id
-output "public_subnet_az1b" {
+output "public_subnet_az1b_id" {
     value = aws_subnet.public_subnet_az1b.id 
 }
 
 # export the private app subnet az1a id
-output "private_app_subnet_az1a" {
+output "private_app_subnet_az1a_id" {
     value = aws_subnet.private_app_subnet_az1a.id  
 }
 
 # export the private app subnet az1b id
-output "private_app_subnet_az1b" {
+output "private_app_subnet_az1b_id" {
     value = aws_subnet.private_app_subnet_az1b.id   
 }
 
 # export the private data subnet az1a id
-output "private_data_subnet_az1a" {
+output "private_data_subnet_az1a_id" {
     value = aws_subnet.private_data_subnet_az1a.id  
 }
 
 # export the private data subnet az1b id
-output "private_data_subnet_az1b" {
+output "private_data_subnet_az1b_id" {
     value = aws_subnet.private_data_subnet_az1b.id   
 }
