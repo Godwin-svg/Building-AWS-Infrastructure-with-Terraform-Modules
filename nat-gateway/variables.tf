0061dffebfa826e2de85d27f0d7e115a715ac13d
Innocent <innocentgodwin580@yahoo.com>
2025-08-20 04:40:52 +0930

    updated

--- nat-gateway/variables.tf
+++ nat-gateway/variables.tf
@@ -1,14 +1,13 @@
-# nat-gateway variables
-# create environment variables
-variable "region" {}
-variable "project_name" {}
-variable "environment" {}
-variable "default_cidr" {}
-variable "vpc_id" {}
-variable "internet_gateway" {}
-variable "public_subnet_az1a_id" {}
-variable "public_subnet_az1b_id" {}
-variable "private_app_subnet_az1a_id" {}
-variable "private_app_subnet_az1b_id" {}
-variable "private_data_subnet_az1a_id" {}
-variable "private_data_subnet_az1b_id" {}
+# # nat-gateway variables
+# # create environment variables
+# variable "region" {}
+# variable "project_name" {}
+# variable "environment" {}
+# variable "default_cidr" {}
+# variable "vpc_id" {}
+# variable "public_subnet_az1a_id" {}
+# variable "public_subnet_az1b_id" {}
+# variable "private_app_subnet_az1a_id" {}
+# variable "private_app_subnet_az1b_id" {}
+# variable "private_data_subnet_az1a_id" {}
+# variable "private_data_subnet_az1b_id" {}
