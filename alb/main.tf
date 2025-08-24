5405492d4797005bf4ea24c11a006c1632221aed
innocent <innocentgodwin580@yahoo.com>
2025-08-25 02:45:08 +0930

    updated

--- alb/main.tf
+++ alb/main.tf
@@ -16,7 +16,7 @@ resource "aws_lb" "application_load_balancer" {
 
 # create target group 
 resource "aws_lb_target_group" "alb_target_group" {
-    name = "${var.project_name}-${var.environment}-alb_target_group"
+    name = lower("${var.project_name}-${var.environment}-alb_target_group")
     port = 80
     protocol = "HTTP"
     vpc_id = var.vpc_id
