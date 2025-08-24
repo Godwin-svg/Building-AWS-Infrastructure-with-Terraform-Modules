b1d5b3de4324743fb4092cf359239289cccb490d
innocent <innocentgodwin580@yahoo.com>
2025-08-24 19:12:32 +0930

    rds module created

---
+++ rds/variable.tf
@@ -0,0 +1,12 @@
+# rds variables
+
+variable "project_name" {}
+variable "environment" {}
+variable "private_data_subnet_az1a_id" {}
+variable "private_data_subnet_az1b_id" {}
+variable "database_snapshot_identifier" {}
+variable "database_instance_class" {}
+variable "availability_zone_1" {}
+variable "database_instance_identifier" {}
+variable "multi_az_deployment" {}
+variable "database_security_group_id" {}
