c0593a66460944b6b58c19c03beda79ca74667b4
innocent <innocentgodwin580@yahoo.com>
2025-08-25 01:03:19 +0930

    created alb module

---
+++ alb/variables.tf
@@ -0,0 +1,10 @@
+# environment variables
+variable "project_name" {}
+variable "environment" {}
+
+# alb variables
+variable "alb_security_group_id" {}
+variable "public_subnet_az1a_id" {}
+variable "public_subnet_az1b_id" {}
+variable "vpc_id" {}
+variable "certificate_arn" {}
