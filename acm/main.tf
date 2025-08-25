47d232330309e559f2e42a8dcc0fb67058eeb212
innocent <innocentgodwin580@yahoo.com>
2025-08-25 15:40:28 +0930

    updated

--- acm/main.tf
+++ acm/main.tf
@@ -11,29 +11,29 @@ resource "aws_acm_certificate" "acm_certificate" {
 }
 
 # # using data source to get details of route 53 hosted zone
-# data "aws_route53_zone" "route53_zone" {
-#     name = var.domain_name
-#     private_zone = false
+data "aws_route53_zone" "route53_zone" {
+    name = var.domain_name
+    private_zone = false
   
-# }
+}
 
-# # create a record set in route 53 for domain validation
-# resource "aws_route53_record" "route53_record" {
-#   for_each = {
-#     for dvo in aws_acm_certificate.acm_certificate.domain_validation_options : dvo.domain_name => {
-#       name   = dvo.resource_record_name
-#       record = dvo.resource_record_value
-#       type   = dvo.resource_record_type
-#     }
-#   }
+# create a record set in route 53 for domain validation
+resource "aws_route53_record" "route53_record" {
+  for_each = {
+    for dvo in aws_acm_certificate.acm_certificate.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
 
-#   allow_overwrite = true
-#   name            = each.value.name
-#   records         = [each.value.record]
-#   ttl             = 60
-#   type            = each.value.type
-#   zone_id         = data.aws_route53_zone.route53_zone.id  
-# }
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.route53_zone.id  
+}
 
 # resource "aws_acm_certificate_validation" "acm_certificate_validation" {
 #   certificate_arn         = aws_acm_certificate.acm_certificate.arn 
